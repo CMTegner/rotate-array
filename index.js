b9262b5f441e7e2842efe601d5b96a4e8d175849
David Ernst <git@dsernst.com>
2015-09-01 14:29:15 -0700

    Adds .all method to return all rotations

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 module.exports = rotate;
-
 function rotate(array, num) {
     num = (num || 0) % array.length;
     if (num < 0) {
@@ -10,4 +9,16 @@ function rotate(array, num) {
     var removed = array.splice(0, num);
     array.push.apply(array, removed);
     return array;
+}
+
+module.exports.all = all;
+function all(array) {
+    var clone = array.slice();
+    var rotations = [];
+    var n;
+    for (n = 0; n < clone.length; n++) {
+        rotations.push(clone.slice());
+        rotate(clone, 1);
+    }
+    return rotations;
 }
